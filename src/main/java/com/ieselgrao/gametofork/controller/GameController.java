7ee7f8f7a2d7f300276aa84554428ea742ff76b0
guilleemp26 <guillemporta26@gmail.com>
2025-11-19 20:32:14 +0100

    Camios en la funcionalidad: aumento de la velocidad de las bolas y aumento en las vidas del usuario

--- src/main/java/com/ieselgrao/gametofork/controller/GameController.java
+++ src/main/java/com/ieselgrao/gametofork/controller/GameController.java
@@ -29,7 +29,7 @@ public class GameController {
     // Parámetros de los círculos
     private final double MIN_RADIUS = 10;
     private final double MAX_RADIUS = 30;
-    private final double FALL_SPEED = 1;
+    private final double FALL_SPEED = 7;
     private final double LOST_LINE_Y = 550; // Línea cerca del pie de la ventana (600px)
 
     @FXML
